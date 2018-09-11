865a5677446502af107e753b43ae42063c449b3e
Stanislav Kalashnik <s.kalashnik@infomir.com>
2018-09-11 14:42:04 +0300

    add build script

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -eu
+
+VERSIONS=(
+    "v342"
+    "v343"
+    "v344"
+)
+
+for version in ${VERSIONS[@]}; do
+    echo $version
+    git clone git@gitlab.infomir.com.ua:web/stbapi.git tmp -b $version
+    cd tmp
+
+    npm i
+    npm run jsdoc:public
+    rsync --recursive --delete ./doc/public/ ../$version
+
+    cd ..
+    rm -rf ./tmp
+done
